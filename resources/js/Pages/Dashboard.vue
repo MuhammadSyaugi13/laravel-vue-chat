9b90bb038efee776ea19f0ade44933cac7eed9de
MuhammadSyaugi13 <muhammadsyaugi13@gmail.com>
2024-07-01 18:03:16 +0700

    test notif

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -13,6 +13,7 @@ const daftarMessage = ref(false)
 const openRoom = (friend_id) => {
 
     friendId.value = friend_id
+    messages.value = []
 
     axios.post(route('room.create'), {
         friend_id,
@@ -21,15 +22,37 @@ const openRoom = (friend_id) => {
         daftarRoom.value = data.dataRoom
         daftarMessage.value = data.dataMessage
 
+        //ambil data chat dari room
+        setMessages()
+
         messageEvent(daftarRoom.value?.id)
 
     }).catch(err => {
-        Toast.fire({
-            icon: 'error',
-            title: `Data:\n${err.message}`
-        })
-        disabledFilter.value = false
+        console.log("error : error get data");
+        console.log(err);
     })
+}
+
+const setMessages = () => {
+
+    if(daftarMessage.value){
+
+        daftarMessage.value.forEach(msg => {
+            if (msg.user !== friendId.value) {
+                messages.value.push({
+                    message: msg.message,
+                    position: "right"
+                })
+            }else{
+                messages.value.push({
+                    message: msg.message,
+                    position: "left"
+                })
+            }
+        });
+
+    }
+
 }
 
 const dataListen = ref('')
@@ -40,7 +63,6 @@ const messageEvent = (roomId) => {
         console.log("join room success");
     })
     .listen('SendMessage', (e)=>{
-        console.log(e);
         dataListen.value = e
         if(e.userId == friendId.value){
 
@@ -52,14 +74,17 @@ const messageEvent = (roomId) => {
     })
     .joining((user) => {
         console.log(user.name);
+        console.log('joiningg');
     })
     .leaving((user) => {
         console.log(user.name);
+        console.log("leaving");
     })
     .error((error) => {
         console.error('error');
         console.error(error);
     });
+
 }
 
 const message = ref('')
@@ -67,13 +92,15 @@ const saveMessage = () => {
 
     if (message.value == "") return;
 
+    scrollToBottom()
+
     messages.value.push({
         message: message.value,
         position: "right"
     })
 
     const messageParam = message.value
-    message.value = messageParam
+    message.value = ""
 
     axios.post(route('chat.save'), {
         roomId: daftarRoom.value?.id,
@@ -82,10 +109,8 @@ const saveMessage = () => {
         message.value = ""
 
     }).catch(err => {
-        Toast.fire({
-            icon: 'error',
-            title: `gagal kirim chat:\n${err.message}`
-        })
+        console.log("error : gagal kirim chat");
+        console.log(err);
     })
 }
 
@@ -98,17 +123,55 @@ const getFriends = () => {
         friends.value = data
         isDaftarFriendsLoading.value = false
     }).catch(err => {
-        Toast.fire({
-            icon: 'error',
-            title: `error getFriends:\n${err.message}`
-        })
+        console.log("error get friends");
+        console.log(err);
         isDaftarFriendsLoading.value = false
     })
 }
 
+const dataFriend = computed(() => {
+
+    // jika tidak ada id teman
+    if (!friendId.value) {
+        return false
+    }
+
+    // jika ada ambil datanya
+    const foundFriend = friends.value.find(obj => obj.id === friendId.value);
+
+    if (foundFriend) {
+        return foundFriend
+    } else {
+        console.log('data teman tidak ada');
+        return false
+    }
+
+})
+
+const scrollableContainer = ref(null)
+
 onMounted(() => {
     getFriends()
 })
+
+// Watch the items array for changes
+
+watch(() => [scrollableContainer.value, messages.value?.length], (to,from) =>{
+
+    scrollToBottom()
+
+    
+})
+
+const scrollToBottom = () => {
+    try {
+        scrollableContainer.value.scrollTop = scrollableContainer.value.scrollHeight
+        scrollableContainer.value = null
+        
+    } catch (error) {
+        
+    }
+}
 
 </script>
 
@@ -137,32 +200,28 @@ onMounted(() => {
                     <h2 class="text-lg font-semibold text-center">Mulai Percakapan</h2>
                 </div>
 
-
                 <!-- Add more conversation items here -->
             </div>
         </div>
 
         <!-- Chat window -->
-        <div class="w-2/3 flex flex-col">
+        <div v-if="friendId"class="w-2/3 flex flex-col align-center justify-center text-center">
             <div class="p-4 border-b border-gray-300">
-                <h2 class="text-xl font-bold">Chat with John Doe</h2>
+                <h2 class="text-xl font-bold">Chat with {{ dataFriend.name }}</h2>
             </div>
+            <div ref="scrollableContainer" v-if="messages.length != 0" class="flex-grow overflow-y-auto p-4">
 
-            <div v-if="messages.length != 0" class="flex-grow overflow-y-auto p-4">
                 <!-- Example chat messages -->
-                 <!--Pesan kanan  -->
                 <div v-for="(d, i) in messages" class="flex mb-4">
+                    <!-- chat user -->
                     <div v-if="d.position == 'right'" class="bg-blue-500 text-white p-3 rounded-lg max-w-xs ml-auto">{{d.message}}</div>
+
+                    <!-- chat friend -->
                     <div v-else class="bg-gray-300 text-gray-800 p-3 rounded-lg max-w-xs">{{d.message}}</div>
                 </div>
 
-                <!--Pesan kiri  -->
-                <!-- <div v-else class="flex mb-4">
-                    <div class="bg-gray-300 text-gray-800 p-3 rounded-lg max-w-xs">{{d.message}}</div>
-                </div> -->
-                <!-- Add more chat messages here -->
             </div>
-            <div v-else class="flex-grow overflow-y-auto p-4">
+            <div  v-else class="flex-grow overflow-y-auto p-4">
                 <div class="flex mb-4">
                     <div class="text-center font-bold text-slate-100 p-3 rounded-lg w-full">Mulai percakapan</div>
                 </div>
@@ -174,6 +233,9 @@ onMounted(() => {
                     <button class="bg-blue-500 text-white p-2 rounded-r-lg">Send</button>
                 </div>
             </div>
+        </div>
+        <div v-else class="flex w-2/3 flex-col align-center justify-center text-center">
+            <div class="text-slate-300 text-lg font-bold">Mulai Chat</div>
         </div>
         <!-- ./ Chat window -->
     </div>
